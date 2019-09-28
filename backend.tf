bb2abc71e4fc528822d0d9dd0c77e1d0a89300fd
bbulla <brent.bulla@nike.com>
2019-09-27 19:22:10 -0700

    Fixing naming convention

--- backend.tf
+++ backend.tf
@@ -2,10 +2,10 @@
 
 terraform {
   backend "s3" {
-    bucket         = "game-fragtopia-us-west-2-389684724582-terraform"
+    bucket         = "game-zombtopia-us-west-2-389684724582-terraform"
     encrypt        = true
     key            = "game-server.tfstate"
     region         = "us-west-2"
-    dynamodb_table = "game-fragtopia-locktable"
+    dynamodb_table = "game-zombtopia-locktable"
   }
 }
